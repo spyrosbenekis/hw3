1e8a2a5dd135fddb6034b97dc5cae76365973b80
thalisap <sdi2300010@di.uoa.gr>
2024-01-29 00:42:09 +0200

    Made basic newton raphson computation program

--- fractal/src/complexlib.h
+++ fractal/src/complexlib.h
@@ -7,3 +7,6 @@ complex cadd(complex n1, complex n2);
 complex csub(complex n1, complex n2);
 complex cmul(complex n1, complex n2);
 complex cdiv(complex n1, complex n2);
+complex crmul(complex n1, double n2);
+complex cpower(complex n, int p);
+double cabsol(complex n);
