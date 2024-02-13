f11ee8f74359f8777a4043f77d18ef9db8c9b0f5
SPYRIDON MPENEKIS <sdi2300132@linux06.di.uoa.gr>
2024-02-13 22:33:44 +0200

    zoomba

--- zoomba/src/zoomba.c
+++ zoomba/src/zoomba.c
@@ -6,15 +6,16 @@
 // Δομή για την αναπαράσταση ενός κόμβου στον αλγόριθμο αναζήτησης
 typedef struct node{
     int x, y;            // Συντεταγμένες του κόμβου
-    int g, h;            
+    int g, h;            // Τιμές g και h
     struct node* parent; // Δείκτης στον γονικό κόμβο
 } node;
 
-// Απόσταση ευκλείδειας
+// Ευκλείδια απόσταση
 double euclidean(int x1, int y1, int x2, int y2){
     return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
 
+// Συνάρτηση f
 int f(node* node){
     return node->g + node->h;
 }
@@ -60,6 +61,7 @@ char* path(node* current){
     return moves;
 }
 
+//Συνάρτηση εύρεσης θέσεων
 void find_moves(int n, int** room, int zoomba_x, int zoomba_y, int target_x, int target_y){
     int dx[] = {-1, 1, 0, 0}; // Πιθανές αλλαγές στον άξονα x
     int dy[] = {0, 0, -1, 1}; // Πιθανές αλλαγές στον άξονα y
@@ -72,31 +74,27 @@ void find_moves(int n, int** room, int zoomba_x, int zoomba_y, int target_x, int
 
     node** closed_set = (node**)malloc(n * n * sizeof(node*)); // Σύνολο αξιολογημένων κόμβων
 
-    bool **array = (bool **)malloc(n * sizeof(bool *));
+    bool **visited = (bool **)malloc(n * sizeof(bool *));
     for(int i = 0; i < n; i++){
-        array[i] = (bool *)calloc(n, sizeof(bool));
+        visited[i] = (bool *)calloc(n * n,sizeof(bool));
     }
 
     open_set[test_size++] = start; // Προσθήκη του αρχικού κόμβου στο σύνολο ανοιχτών κόμβων
-
-    int k;
 
     while(test_size > 0){
 
         node* current = find_min_f(open_set, test_size); // Εύρεση κόμβου με ελάχιστη τιμή f
 
-        if (current->x == goal->x && current->y == goal->y){ // Εάν έχει επιτευχθεί ο στόχος
-            char* moves = path(current); // Ανακατασκευή μονοπατιού
-            printf("%s\n", moves); // Εκτύπωση κινήσεων
-            return;
-        }
-
         closed_set[test_size - 1] = current;
-        array[current->x][current->y] = test_size - 1;
-
+        visited[current->x][current->y] = true;
+        
         // Αφαίρεση του τρέχοντος κόμβου από το σύνολο ανοιχτών κόμβων
-        k = array[current->x][current->y];
-        open_set[k] = open_set[test_size-1];
+        int k;
+        for (k = 0; k < test_size; ++k) {
+            if (open_set[k] == current)
+                break;
+        }
+        open_set[k] = open_set[test_size - 1];
         test_size--;
 
         // Δημιουργία διαδόχων κόμβων και προσθήκη τους στο σύνολο ανοιχτών κόμβων
@@ -105,24 +103,35 @@ void find_moves(int n, int** room, int zoomba_x, int zoomba_y, int target_x, int
             int nx = current->x + dx[i];
             int ny = current->y + dy[i];
 
-            if(nx >= 0 && nx < n && ny >= 0 && ny < n && room[nx][ny] == 0){
+            if(nx >= 0 && nx < n && ny >= 0 && ny < n && room[nx][ny] == 0 && !visited[nx][ny]){
 
                 node* successor = create_node(nx, ny, current->g + 1, euclidean(nx, ny, target_x, target_y), current);
 
-                bool skip_successor = false;
-                k = array[successor->x][successor->y];
-                if(k != 0 && closed_set[k] != NULL){
-                    if(f(successor) > f(closed_set[k])) {
-                        skip_successor = true;
-                        break;
+                if(successor->x == goal->x && successor->y == goal->y){ // Εάν έχει επιτευχθεί ο στόχος
+                    char* moves = path(successor); // Ανακατασκευή μονοπατιού
+                    printf("%s\n", moves); // Εκτύπωση κινήσεων
+                    free(moves);
+                    free(successor);
+                    return;
+                }
+                else{
+                    int f2 = f(successor);
+                    for (k = 0; k < test_size; ++k) {
+                        if (open_set[k] && open_set[k]->x == successor->x && open_set[k]->y == successor->y){
+                            if(f2 >= f(open_set[k])) {
+                                visited[successor->x][successor->y] = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!visited[successor->x][successor->y]){
+                        open_set[test_size++] = successor;
+                        visited[successor->x][successor->y] = true;
+                    }
+                    else{
+                        free(successor);
                     }
                 }
-                if (!skip_successor){
-                    open_set[test_size++] = successor;
-                    array[successor->x][successor->y] = test_size - 1;
-                }
-                else
-                    free(successor);
             }
         }
     }
@@ -131,13 +140,14 @@ void find_moves(int n, int** room, int zoomba_x, int zoomba_y, int target_x, int
 
     // Απελευθέρωση μνήμης
     for (int i = 0; i < n; i++) {
-        free(array[i]);
+        free(visited[i]);
     }
-    free(array);
+    free(visited);
 }
 
 // Κύρια συνάρτηση
 int main(){
+
     int n;
     if(scanf("%d", &n) != 1){
         return 1;
@@ -148,9 +158,12 @@ int main(){
     }
 
     int zoomba_x, zoomba_y, target_x, target_y;
-    if(scanf("%d %d %d %d", &zoomba_x, &zoomba_y, &target_x, &target_y) != 4 ||
-    zoomba_x < 0 || zoomba_x >= n || zoomba_y < 0 || zoomba_y >= n ||
-    target_x < 0 || target_x >= n || target_y < 0 || target_y >= n){
+    if(scanf("%d %d %d %d", &zoomba_x, &zoomba_y, &target_x, &target_y) != 4){
+        return 1;
+    }
+    
+    if(zoomba_x < 0 || zoomba_x >= n || zoomba_y < 0 || zoomba_y >= n ||
+       target_x < 0 || target_x >= n || target_y < 0 || target_y >= n){
         return 1;
     }
 
